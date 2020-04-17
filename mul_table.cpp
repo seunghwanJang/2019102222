220ef63af61e4378911d8197232392e4508aebcb
seoh02h <seok02h@khu.ac.kr>
2020-04-17 18:58:54 +0900

    Modified

--- mul_table.cpp
+++ mul_table.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
 	cout << "print mul table_________________" << endl;
-	for (int i = 1; i < 9; i++)
+	for (int i = 1; i < 10; i++)
 	{
 		for (int j = 1; j < 10; j++)
 			cout << "\t" << i << " X " << j << " = " << i * j << endl;
